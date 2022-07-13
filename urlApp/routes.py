e9b33dffbbeacc69756e764c8c282f8b576a09bd
Raghavendra Khare <raghavxk@gmail.com>
2022-07-13 11:39:19 +0530

    updates to add url handler

--- urlApp/routes.py
+++ urlApp/routes.py
@@ -1,33 +1,50 @@
 from database.repository.short_urls import UrlStoreRepository
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, status
+from fastapi.responses import JSONResponse
+from requests import Response
+from utils import hash
 
 from urlApp import schemas
-from utils import hash
 
 router = APIRouter(prefix="/url", tags=["URLS"])
 
 
-@router.post("", status_code=status.HTTP_201_CREATED)
+@router.post("")
 def handler_create_url(
         req: schemas.PostReqBody):
     """
-    Route : GET /url
+    Route : POST /url
     -> generates short url codes for long urls
     """
     try:
         shortened_url_code = hash.generate_hash(req.url)
+
         saved_data = UrlStoreRepository().save_new_url(
             short_url_code=shortened_url_code, original_url=req.url)
-        return {
-            'info': {
+
+        if not saved_data:
+            return JSONResponse(
+                content={
+                    'success': False,
+                    'msg': 'can not register an already registered-url'
+                },
+                status_code=status.HTTP_406_NOT_ACCEPTABLE
+            )
+
+        return JSONResponse(
+            content={'info': {
                 'originalUrl': req.url,
                 'shortenedUrlCode': shortened_url_code
             },
-            'success': True
-        }
+                'success': True
+            },
+            status_code=status.HTTP_201_CREATED)
+
     except Exception as e:
         print(e)
-        raise HTTPException(
-            status_code=status.HTTP_406_NOT_ACCEPTABLE,
-            detail=f"failed to generate short url"
+        return JSONResponse(content={
+            'success': False,
+            'msg': 'failed to register url'
+        },
+            status_code=status.HTTP_406_NOT_ACCEPTABLE
         )
